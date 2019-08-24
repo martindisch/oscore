5a3f8f4231a9d521ece27cbed4c7f846740ced5d
Martin Disch <martindisch@gmail.com>
2019-08-24 14:31:26 +0200

    Add test vector for C_2

--- src/edhoc/util.rs
+++ src/edhoc/util.rs
@@ -678,51 +678,29 @@ mod tests {
         assert_eq!(&V_SIG[..], &sig[..]);
     }
 
-    const AEAD_SECRET_KEY: [u8; CCM_KEY_LEN] = [
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA,
-        0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-    ];
-    const AEAD_NONCE: [u8; CCM_NONCE_LEN] = [
-        0x00, 0x00, 0x00, 0x03, 0x02, 0x01, 0x00, 0xA0, 0xA1, 0xA2, 0xA3,
-        0xA4, 0xA5,
-    ];
-    const AEAD_PT: [u8; 23] = [
-        0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12,
-        0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D,
-        0x1E,
-    ];
-    const AEAD_AD: [u8; 8] = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
-
     #[test]
     fn aead() {
-        // Check for plaintext equality
-        let ct = aead_seal(&AEAD_SECRET_KEY, &AEAD_NONCE, &AEAD_PT, &AEAD_AD)
-            .unwrap();
-        let pt =
-            aead_open(&AEAD_SECRET_KEY, &AEAD_NONCE, &ct, &AEAD_AD).unwrap();
-        assert_eq!(&AEAD_PT[..], &pt[..]);
+        // Check encryption
+        let ct = aead_seal(&K_2, &IV_2, &P_2, &A_2).unwrap();
+        assert_eq!(&C_2[..], &ct[..]);
+
+        // Check decryption
+        let pt = aead_open(&K_2, &IV_2, &C_2, &A_2).unwrap();
+        assert_eq!(&P_2[..], &pt[..]);
 
         // Check verification fail on manipulated ciphertext
         let mut ct_manip = ct.clone();
         ct_manip[2] = 0x00;
-        assert!(
-            aead_open(&AEAD_SECRET_KEY, &AEAD_NONCE, &ct_manip, &AEAD_AD)
-                .is_err()
-        );
+        assert!(aead_open(&K_2, &IV_2, &ct_manip, &A_2).is_err());
 
         // Check verification fail on manipulated tag
         let mut ct_manip = ct.clone();
-        ct_manip[AEAD_PT.len() + 4] = 0x00;
-        assert!(
-            aead_open(&AEAD_SECRET_KEY, &AEAD_NONCE, &ct_manip, &AEAD_AD)
-                .is_err()
-        );
+        ct_manip[P_2.len() + 4] = 0x00;
+        assert!(aead_open(&K_2, &IV_2, &ct_manip, &A_2).is_err());
 
         // Check verification fail on wrong AD
-        let mut ad_manip = AEAD_AD.to_vec();
+        let mut ad_manip = A_2.to_vec();
         ad_manip[6] = 0x00;
-        assert!(
-            aead_open(&AEAD_SECRET_KEY, &AEAD_NONCE, &ct, &ad_manip).is_err()
-        );
+        assert!(aead_open(&K_2, &IV_2, &C_2, &ad_manip).is_err());
     }
 }
