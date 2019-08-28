382b0dd3ca6c817a61b7383e3a5124fc3832de32
Martin Disch <martindisch@gmail.com>
2019-08-28 17:01:08 +0200

    Flesh out the message trait
    Still nowhere near good, but might be workable now. For getting the
    options, the iterator solution would be nicer, although if we don't
    want to mutate the options while we go over them (which would require
    additional implementation on the user's part), we'll probably still
    have to take the iterator, copy the options into a collection and then
    modify them after the fact. Not much better than what we're doing now.

--- src/coap_message.rs
+++ src/coap_message.rs
@@ -1,49 +1,61 @@
+//! Trait and types that allow for interoperability with CoAP implementations.
+
 use alloc::vec::Vec;
 
-#[derive(Debug)]
-pub enum MethodCode {
-    Get,
+/// CoAP method/response codes used by OSCORE.
+///
+/// Only the ones we need to know because we set them for requests (`POST`) and
+/// responses (`CHANGED`) are listed here. The others are wrapped in `Other` and
+/// protected (E).
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum CoapCode {
     Post,
-    Put,
-    Delete,
-    UnKnown,
+    Changed,
+    Other(usize),
 }
 
-#[derive(Debug)]
-pub enum OptionCode {
-    IfMatch,
+/// CoAP option codes used by OSCORE.
+///
+/// Only the ones we need to know because they're either unprotected (U) or
+/// optional and unsupported by our implementation are listed here.
+/// All the others are wrapped in `Other` and protected (E) by default.
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum CoapOption {
     UriHost,
-    ETag,
-    IfNoneMatch,
     Observe,
     UriPort,
-    LocationPath,
-    UriPath,
-    ContentFormat,
-    MaxAge,
-    UriQuery,
-    Accept,
-    LocationQuery,
     Block2,
     Block1,
+    Size2,
     ProxyUri,
     ProxyScheme,
     Size1,
-    Size2,
     NoResponse,
+    Other(usize),
 }
 
-pub trait CoapOption {
-    fn delete(&mut self);
-}
-
+/// This needs to be implemented for the CoAP message type of the CoAP
+/// implementation for which messages are to be protected with OSCORE.
 pub trait CoapMessage {
-    type Options: Iterator<Item = OptionCode>;
+    /// Returns the method/response code from the header.
+    fn get_code(&self) -> CoapCode;
 
-    fn options(&self) -> Self::Options;
+    /// Sets the method/response code of the header.
+    fn set_code(&mut self, code: CoapCode);
 
-    fn set_method(&mut self, method: MethodCode);
+    /// Returns a vector of all options present in the message.
+    ///
+    /// It doesn't matter if repeatable options occur once or multiple times,
+    /// choose what's more convenient based on the underlying implementation.
+    fn options(&self) -> Vec<CoapOption>;
 
-    fn add_option(&mut self, option: OptionCode, value: Vec<u8>);
-    fn delete_option(&mut self, option: OptionCode);
+    /// Adds an option to the message.
+    ///
+    /// Can be called multiple times for repeatable options.
+    fn add_option(&mut self, option: CoapOption, value: Vec<u8>);
+
+    /// Deletes an option from the message.
+    ///
+    /// For repeatable options, this deletes *all* occurrences.
+    fn delete_option(&mut self, option: CoapOption);
 }
