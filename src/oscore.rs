82f4da2e819d264300c150193ebfc319701f0e24
Martin Disch <martindisch@gmail.com>
2019-09-01 09:39:52 +0200

    Implement AAD array creation

--- src/oscore.rs
+++ src/oscore.rs
@@ -124,6 +124,23 @@ fn hkdf(
     Ok(okm)
 }
 
+/// Returns the CBOR encoded AAD array.
+///
+/// There's no argument for class I options, because the standard doesn't
+/// define any at this point.
+fn build_aad_array(request_kid: &[u8], request_piv: &[u8]) -> Result<Vec<u8>> {
+    // (oscore_version, algorithms, request_kid, request_piv, options)
+    let arr = (
+        1,
+        [10],
+        Bytes::new(request_kid),
+        Bytes::new(request_piv),
+        Bytes::new(&[]),
+    );
+    // Return the CBOR encoded version of that
+    cbor::encode(arr)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -154,6 +171,16 @@ mod tests {
         0x46, 0x22, 0xD4, 0xDD, 0x6D, 0x94, 0x41, 0x68, 0xEE, 0xFB, 0x54,
         0x98, 0x7C,
     ];
+
+    const EXAMPLE_KID: [u8; 1] = [0x00];
+    const EXAMPLE_PIV: [u8; 1] = [0x25];
+    const EXAMPLE_AAD_ARR: [u8; 9] =
+        [0x85, 0x01, 0x81, 0x0A, 0x41, 0x00, 0x41, 0x25, 0x40];
+
+    const VECTOR_KID: [u8; 0] = [];
+    const VECTOR_PIV: [u8; 1] = [0x14];
+    const VECTOR_AAD_ARR: [u8; 8] =
+        [0x85, 0x01, 0x81, 0x0A, 0x40, 0x41, 0x14, 0x40];
 
     #[test]
     fn info() {
@@ -203,5 +230,16 @@ mod tests {
             &security_context.recipient_context.recipient_key[..]
         );
         assert_eq!(0, security_context.recipient_context.replay_window);
+    }
+
+    #[test]
+    fn aad_array() {
+        let example_aad_arr =
+            build_aad_array(&EXAMPLE_KID, &EXAMPLE_PIV).unwrap();
+        assert_eq!(&EXAMPLE_AAD_ARR, &example_aad_arr[..]);
+
+        let vector_aad_arr =
+            build_aad_array(&VECTOR_KID, &VECTOR_PIV).unwrap();
+        assert_eq!(&VECTOR_AAD_ARR, &vector_aad_arr[..]);
     }
 }
